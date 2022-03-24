ccb12088af9214afa32cf5c116b74e35f62bd2e2
Jacob Chan <44072996+huskychanct@users.noreply.github.com>
2022-03-24 00:07:28 -0400

    Main method placement update

--- Car.java
+++ Car.java
@@ -8,13 +8,6 @@ public class Car {
     public void driveFast() {
         System.out.println("I am going " + top_speed);
     }
-
-    public static void main(String[] args) {
-        Lambo lambo_1 = new Lambo();
-        Truck truck_1 = new Truck();
-        lambo_1.driveFast();
-        truck_1.driveFast();
-    }
 }
 
 class Lambo extends Car {
@@ -29,3 +22,11 @@ class Truck extends Car {
     }
 }
 
+class Runner{
+    public static void main(String[] args) {
+        Lambo lambo_1 = new Lambo();
+        Truck truck_1 = new Truck();
+        lambo_1.driveFast();
+        truck_1.driveFast();
+    }
+}
